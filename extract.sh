ad7265abe366c99a306c26290f13f526550ef56d
Avishek Dutta <35264497+avishekdutta14@users.noreply.github.com>
2021-09-14 08:58:06 -0700

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -4,15 +4,15 @@
 
 bin=$1
 
-#Extracting header of fasta sequence
+# Extracting header of fasta sequence
 
 grep -e ">" $bin.fa > header.txt
 
-#Removing > from each line
+# Removing > from each line
 
 awk 'sub(/^>/, "")' header.txt > header1.txt
 
-#adding file name after each line
+# adding file name after each line
 
 cat header1.txt | while read line; do echo ${line},$bin; done >> contig_fate.txt
 
